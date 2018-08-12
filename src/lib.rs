124006292ed640d2a0c7c9fa9d5cea30914f13ca
Nick Babcock <nbabcock19@hotmail.com>
2018-08-12 17:05:19 -0500

    Remove explicit state class

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,8 @@ use byteorder::{ByteOrder, LE};
 use std::num::Wrapping;
 use std::ops::Index;
 
+#[derive(Default, Clone)]
 pub struct Key([u64; 4]);
-
-#[derive(Default)]
-struct PortableState {
-    v0: [u64; 4],
-    v1: [u64; 4],
-    mul0: [u64; 4],
-    mul1: [u64; 4],
-}
 
 impl Index<usize> for Key {
     type Output = u64;
@@ -23,34 +16,24 @@ impl Index<usize> for Key {
     }
 }
 
-impl PortableState {
-    fn reset(&mut self, key: &Key) {
-        self.mul0[0] = 0xdbe6d5d5fe4cce2f;
-        self.mul0[1] = 0xa4093822299f31d0;
-        self.mul0[2] = 0x13198a2e03707344;
-        self.mul0[3] = 0x243f6a8885a308d3;
-        self.mul1[0] = 0x3bd39e10cb0ef593;
-        self.mul1[1] = 0xc0acf169b5f18a8c;
-        self.mul1[2] = 0xbe5466cf34e90c6c;
-        self.mul1[3] = 0x452821e638d01377;
-        self.v0[0] = self.mul0[0] ^ key[0];
-        self.v0[1] = self.mul0[1] ^ key[1];
-        self.v0[2] = self.mul0[2] ^ key[2];
-        self.v0[3] = self.mul0[3] ^ key[3];
-        self.v1[0] = self.mul1[0] ^ ((key[0] >> 32) | (key[0] << 32));
-        self.v1[1] = self.mul1[1] ^ ((key[1] >> 32) | (key[1] << 32));
-        self.v1[2] = self.mul1[2] ^ ((key[2] >> 32) | (key[2] << 32));
-        self.v1[3] = self.mul1[3] ^ ((key[3] >> 32) | (key[3] << 32));
-    }
+#[derive(Default)]
+pub struct PortableHash {
+    key: Key,
+    v0: [u64; 4],
+    v1: [u64; 4],
+    mul0: [u64; 4],
+    mul1: [u64; 4],
 }
 
-pub struct PortableHash;
-
 impl PortableHash {
+    pub fn new(key: &Key) -> Self {
+        PortableHash { key: key.clone(), ..Default::default() }
+    }
+
     pub fn hash64(data: &[u8], key: &Key) -> u64 {
-        let mut state = PortableState::default();
-        PortableHash::process_all(data, key, &mut state);
-        PortableHash::finalize64(&mut state)
+        let mut hash = PortableHash::new(key);
+        hash.process_all(data);
+        hash.finalize64()
     }
 
     pub fn hash128(data: &[u8], key: &Key) -> u128 {
@@ -61,15 +44,34 @@ impl PortableHash {
         (0, 0)
     }
 
-    fn finalize64(state: &mut PortableState) -> u64 {
+    fn reset(&mut self) {
+        self.mul0[0] = 0xdbe6d5d5fe4cce2f;
+        self.mul0[1] = 0xa4093822299f31d0;
+        self.mul0[2] = 0x13198a2e03707344;
+        self.mul0[3] = 0x243f6a8885a308d3;
+        self.mul1[0] = 0x3bd39e10cb0ef593;
+        self.mul1[1] = 0xc0acf169b5f18a8c;
+        self.mul1[2] = 0xbe5466cf34e90c6c;
+        self.mul1[3] = 0x452821e638d01377;
+        self.v0[0] = self.mul0[0] ^ self.key[0];
+        self.v0[1] = self.mul0[1] ^ self.key[1];
+        self.v0[2] = self.mul0[2] ^ self.key[2];
+        self.v0[3] = self.mul0[3] ^ self.key[3];
+        self.v1[0] = self.mul1[0] ^ ((self.key[0] >> 32) | (self.key[0] << 32));
+        self.v1[1] = self.mul1[1] ^ ((self.key[1] >> 32) | (self.key[1] << 32));
+        self.v1[2] = self.mul1[2] ^ ((self.key[2] >> 32) | (self.key[2] << 32));
+        self.v1[3] = self.mul1[3] ^ ((self.key[3] >> 32) | (self.key[3] << 32));
+    }
+
+    fn finalize64(&mut self) -> u64 {
         for i in 0..4 {
-            PortableHash::permute_and_update(state);
+            self.permute_and_update();
         }
 
-        state.v0[0]
-            .wrapping_add(state.v1[0])
-            .wrapping_add(state.mul0[0])
-            .wrapping_add(state.mul1[0])
+        self.v0[0]
+            .wrapping_add(self.v1[0])
+            .wrapping_add(self.mul0[0])
+            .wrapping_add(self.mul1[0])
     }
 
     fn permute(v: &[u64; 4]) -> [u64; 4] {
@@ -81,23 +83,23 @@ impl PortableHash {
         ]
     }
 
-    fn permute_and_update(state: &mut PortableState) {
-        let permuted: [u64; 4] = PortableHash::permute(&state.v0);
-        PortableHash::update(permuted, state)
+    fn permute_and_update(&mut self) {
+        let permuted: [u64; 4] = PortableHash::permute(&self.v0);
+        self.update(permuted)
     }
 
-    fn update(lanes: [u64; 4], state: &mut PortableState) {
+    fn update(&mut self, lanes: [u64; 4]) {
         for i in 0..4 {
-            state.v1[i] = state.v1[i].wrapping_add(state.mul0[i].wrapping_add(lanes[i]));
-            state.mul0[i] ^= (state.v1[i] & 0xffffffff).wrapping_mul(state.v0[i] >> 32);
-            state.v0[i] = state.v0[i].wrapping_add(state.mul1[i]);
-            state.mul1[i] ^= (state.v0[i] & 0xffffffff).wrapping_mul(state.v1[i] >> 32);
+            self.v1[i] = self.v1[i].wrapping_add(self.mul0[i].wrapping_add(lanes[i]));
+            self.mul0[i] ^= (self.v1[i] & 0xffffffff).wrapping_mul(self.v0[i] >> 32);
+            self.v0[i] = self.v0[i].wrapping_add(self.mul1[i]);
+            self.mul1[i] ^= (self.v0[i] & 0xffffffff).wrapping_mul(self.v1[i] >> 32);
         }
 
-        PortableHash::zipper_merge_and_add(state.v1[1], state.v1[0], &mut state.v0, 1, 0);
-        PortableHash::zipper_merge_and_add(state.v1[3], state.v1[2], &mut state.v0, 3, 2);
-        PortableHash::zipper_merge_and_add(state.v0[1], state.v0[0], &mut state.v1, 1, 0);
-        PortableHash::zipper_merge_and_add(state.v0[3], state.v0[2], &mut state.v1, 3, 2);
+        PortableHash::zipper_merge_and_add(self.v1[1], self.v1[0], &mut self.v0, 1, 0);
+        PortableHash::zipper_merge_and_add(self.v1[3], self.v1[2], &mut self.v0, 3, 2);
+        PortableHash::zipper_merge_and_add(self.v0[1], self.v0[0], &mut self.v1, 1, 0);
+        PortableHash::zipper_merge_and_add(self.v0[3], self.v0[2], &mut self.v1, 3, 2);
     }
 
     fn zipper_merge_and_add(v1: u64, v0: u64, lane: &mut [u64; 4], add1: usize, add0: usize) {
@@ -120,26 +122,27 @@ impl PortableHash {
         );
     }
 
-    fn update_packet(packet: &[u8], state: &mut PortableState) {
+    fn update_packet(&mut self, packet: &[u8]) {
         let lanes: [u64; 4] = [
             LE::read_u64(&packet[0..8]),
             LE::read_u64(&packet[8..16]),
             LE::read_u64(&packet[16..24]),
             LE::read_u64(&packet[24..32]),
         ];
-        PortableHash::update(lanes, state);
+
+        self.update(lanes);
     }
 
-    fn process_all(data: &[u8], key: &Key, state: &mut PortableState) {
-        state.reset(key);
+    fn process_all(&mut self, data: &[u8]) {
+        self.reset();
         let mut slice = &data[..];
         while slice.len() >= 32 {
-            PortableHash::update_packet(&slice, state);
+            self.update_packet(&slice);
             slice = &slice[32..];
         }
 
         if (!slice.is_empty()) {
-            PortableHash::update_remainder(&slice, state);
+            self.update_remainder(&slice);
         }
     }
 
@@ -152,7 +155,7 @@ impl PortableHash {
         }
     }
 
-    fn update_remainder(bytes: &[u8], state: &mut PortableState) {
+    fn update_remainder(&mut self, bytes: &[u8]) {
         let size_mod4 = bytes.len() & 3;
         let remainder_jump = bytes.len() & !3;
         let remainder = &bytes[remainder_jump..];
@@ -160,10 +163,10 @@ impl PortableHash {
         let mut packet: [u8; 32] = Default::default();
 
         for i in 0..4 {
-            state.v0[i] += (size << 32) + size;
+            self.v0[i] += (size << 32) + size;
         }
 
-        PortableHash::rotate_32_by(size, &mut state.v1);
+        PortableHash::rotate_32_by(size, &mut self.v1);
         packet[..remainder_jump].clone_from_slice(&bytes[..remainder_jump]);
         if (size & 16 != 0) {
             for i in 0..4 {
@@ -174,7 +177,8 @@ impl PortableHash {
             packet[16 + 1] = remainder[size_mod4 >> 1];
             packet[16 + 2] = remainder[size_mod4 - 1];
         }
-        PortableHash::update_packet(&packet, state);
+
+        self.update_packet(&packet);
     }
 }
 
