5ded6f71e9b9fb1b0857ba5a5d5d73971cdc3137
Nick Babcock <nbabcock19@hotmail.com>
2018-08-16 07:07:51 -0500

    Add sha256 benchmark

--- benches/highway.rs
+++ benches/highway.rs
@@ -1,9 +1,11 @@
 #[macro_use]
 extern crate criterion;
 extern crate highway;
+extern crate sha2;
 
 use criterion::{Criterion, ParameterizedBenchmark, Throughput};
 use highway::{AvxHash, Key, PortableHash, SseHash};
+use sha2::{Sha256, Digest};
 
 fn hashing(c: &mut Criterion) {
     let parameters = vec![1, 4, 16, 64, 256, 1024, 4096, 16384, 65536];
@@ -17,18 +19,40 @@ fn hashing(c: &mut Criterion) {
                 let key = Key([0, 0, 0, 0]);
                 b.iter(|| PortableHash::hash64(&data, &key))
             },
-            parameters,
+            parameters.clone(),
         ).with_function("sse", |b, param| {
             let data = vec![0u8; *param];
             let key = Key([0, 0, 0, 0]);
             b.iter(|| SseHash::hash64(&data, &key))
-        })
-            .with_function("avx", |b, param| {
+        }).with_function("avx", |b, param| {
+            let data = vec![0u8; *param];
+            let key = Key([0, 0, 0, 0]);
+            b.iter(|| AvxHash::hash64(&data, &key))
+        }).throughput(|s| Throughput::Bytes(*s as u32)),
+    );
+
+    c.bench(
+        "256bit",
+        ParameterizedBenchmark::new(
+            "portable",
+            |b, param| {
                 let data = vec![0u8; *param];
                 let key = Key([0, 0, 0, 0]);
-                b.iter(|| AvxHash::hash64(&data, &key))
-            })
-            .throughput(|s| Throughput::Bytes(*s as u32)),
+                b.iter(|| PortableHash::hash256(&data, &key))
+            },
+            parameters.clone(),
+        ).with_function("sse", |b, param| {
+            let data = vec![0u8; *param];
+            let key = Key([0, 0, 0, 0]);
+            b.iter(|| SseHash::hash256(&data, &key))
+        }).with_function("avx", |b, param| {
+            let data = vec![0u8; *param];
+            let key = Key([0, 0, 0, 0]);
+            b.iter(|| AvxHash::hash256(&data, &key))
+        }).with_function("sha2", |b, param| {
+            let data = vec![0u8; *param];
+            b.iter(|| Sha256::digest(&data))
+        }).throughput(|s| Throughput::Bytes(*s as u32)),
     );
 }
 
