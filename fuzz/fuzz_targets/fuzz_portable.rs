24af1472ad657fc60285dff24d7d45bfc241ff25
Nick Babcock <nbabcock19@hotmail.com>
2018-09-16 16:58:24 +0000

    Start of fuzzing

---
+++ fuzz/fuzz_targets/fuzz_portable.rs
@@ -0,0 +1,8 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate highway;
+use highway::{HighwayHash, Key, PortableHash};
+
+fuzz_target!(|data: &[u8]| {
+    PortableHash::new(&Key([1, 2, 3, 4])).hash64(data);
+});
