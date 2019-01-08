91d841e7cdfe73034e8402e0fc0b8434e36a9520
Nick Babcock <nbabcock19@hotmail.com>
2019-01-07 19:03:25 -0600

    Property test all hash impls against each other

--- tests/properties.rs
+++ tests/properties.rs
@@ -46,6 +46,69 @@ mod quick_tests {
             let hash2 = HighwayBuilder::new(&key).hash256(data.as_slice());
             hash1 == hash2
         }
+
+        fn all64_eq(k1: u64, k2: u64, k3: u64, k4: u64, data: Vec<u8>) -> bool {
+            let key = Key([k1, k2, k3, k4]);
+            let hash1 = PortableHash::new(&key).hash64(data.as_slice());
+            let hash2 = HighwayBuilder::new(&key).hash64(data.as_slice());
+            let mut res = hash1 == hash2;
+
+            #[cfg(target_arch = "x86_64")]
+            {
+                use highway::{AvxHash, SseHash};
+                if let Some(h) = AvxHash::new(&key) {
+                    res &= h.hash64(data.as_slice()) == hash1;
+                }
+
+                if let Some(h) = SseHash::new(&key) {
+                    res &= h.hash64(data.as_slice()) == hash1;
+                }
+            }
+
+            res
+        }
+
+        fn all128_eq(k1: u64, k2: u64, k3: u64, k4: u64, data: Vec<u8>) -> bool {
+            let key = Key([k1, k2, k3, k4]);
+            let hash1 = PortableHash::new(&key).hash128(data.as_slice());
+            let hash2 = HighwayBuilder::new(&key).hash128(data.as_slice());
+            let mut res = hash1 == hash2;
+
+            #[cfg(target_arch = "x86_64")]
+            {
+                use highway::{AvxHash, SseHash};
+                if let Some(h) = AvxHash::new(&key) {
+                    res &= h.hash128(data.as_slice()) == hash1;
+                }
+
+                if let Some(h) = SseHash::new(&key) {
+                    res &= h.hash128(data.as_slice()) == hash1;
+                }
+            }
+
+            res
+        }
+
+        fn all256_eq(k1: u64, k2: u64, k3: u64, k4: u64, data: Vec<u8>) -> bool {
+            let key = Key([k1, k2, k3, k4]);
+            let hash1 = PortableHash::new(&key).hash256(data.as_slice());
+            let hash2 = HighwayBuilder::new(&key).hash256(data.as_slice());
+            let mut res = hash1 == hash2;
+
+            #[cfg(target_arch = "x86_64")]
+            {
+                use highway::{AvxHash, SseHash};
+                if let Some(h) = AvxHash::new(&key) {
+                    res &= h.hash256(data.as_slice()) == hash1;
+                }
+
+                if let Some(h) = SseHash::new(&key) {
+                    res &= h.hash256(data.as_slice()) == hash1;
+                }
+            }
+
+            res
+        }
     }
 }
 
