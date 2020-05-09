e4595932bd2bfe64d9057a46ea68dd017a708bb2
Nick Babcock <nbabcock19@hotmail.com>
2020-05-09 10:15:31 -0500

    Implement fuzz arbitrary by hand

--- fuzz/fuzz_targets/common.rs
+++ fuzz/fuzz_targets/common.rs
@@ -1,7 +1,17 @@
 use highway::Key;
+use libfuzzer_sys::arbitrary::{Arbitrary, Result, Unstructured};
 
-#[derive(Debug, arbitrary::Arbitrary)]
+#[derive(Debug)]
 pub struct FuzzKey {
     pub key: Key,
     pub data: Vec<u8>,
+}
+
+impl Arbitrary for FuzzKey {
+    fn arbitrary(u: &mut Unstructured<'_>) -> Result<Self> {
+        let d = <[u64; 4]>::arbitrary(u)?;
+        let key = Key(d);
+        let data = <Vec<u8>>::arbitrary(u)?;
+        Ok(FuzzKey { key, data })
+    }
 }
