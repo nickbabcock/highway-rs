267b50d7bfc62597013f94bef905187f2e0019b9
Nick Babcock <nbabcock19@hotmail.com>
2021-03-13 13:32:43 -0600

    Fuzz against reference implementation
    This should make it much clearer to others that all implementations
    produce the same output.

--- fuzz/fuzz_targets/fuzz_highway.rs
+++ fuzz/fuzz_targets/fuzz_highway.rs
@@ -1,7 +1,12 @@
 #![no_main]
 
-use highway::{AvxHash, HighwayHash, Key, PortableHash, SseHash};
+use highway::{AvxHash, HighwayHash, HighwayHasher, Key, PortableHash, SseHash};
+use libc::size_t;
 use libfuzzer_sys::arbitrary;
+
+extern "C" {
+    fn HighwayHash64(data: *const u8, size: size_t, key: *const u64) -> u64;
+}
 
 #[derive(Debug, arbitrary::Arbitrary)]
 pub struct FuzzKey {
@@ -16,8 +21,19 @@ libfuzzer_sys::fuzz_target!(|input: FuzzKey| {
     for hash in &mut hashes {
         let portable64 = PortableHash::new(key).hash64(data);
         *hash = portable64;
+        let expected = unsafe { HighwayHash64(data.as_ptr(), data.len(), input.key.as_ptr()) };
+        assert_eq!(portable64, expected);
+
         let portable128 = PortableHash::new(key).hash128(data);
         let portable256 = PortableHash::new(key).hash256(data);
+
+        let builder64 = HighwayHasher::new(key).hash64(data);
+        let builder128 = HighwayHasher::new(key).hash128(data);
+        let builder256 = HighwayHasher::new(key).hash256(data);
+
+        assert_eq!(builder64, portable64);
+        assert_eq!(builder128, portable128);
+        assert_eq!(builder256, portable256);
 
         if let Some(hash) = AvxHash::new(key).map(|x| x.hash64(data)) {
             assert_eq!(hash, portable64)
