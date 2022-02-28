48140bc1c6055de6294e7a8727c6834c6f778459
Nick Babcock <nbabcock19@hotmail.com>
2022-02-28 08:21:41 -0600

    Remove x86 macro from top level module

--- src/macros.rs
+++ src/macros.rs
@@ -1,14 +1,3 @@
-#[cfg(target_arch = "x86_64")]
-/// The function, [_MM_SHUFFLE](https://doc.rust-lang.org/core/arch/x86_64/fn._MM_SHUFFLE.html) is
-/// only supported on nightly and there has been [some controversy
-/// around](https://github.com/rust-lang-nursery/stdsimd/issues/522) it regarding the type
-/// signature, so the safe route here is to just go with our own macro.
-macro_rules! _mm_shuffle {
-    ($z:expr, $y:expr, $x:expr, $w:expr) => {
-        ($z << 6) | ($y << 4) | ($x << 2) | $w
-    };
-}
-
 macro_rules! impl_write {
     ($hasher_struct:ty) => {
         #[cfg(feature = "std")]
