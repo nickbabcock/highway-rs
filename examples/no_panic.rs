ff78e113d84f3459eb3486f7ed17172470e18f72
Nick Babcock <nbabcock19@hotmail.com>
2024-01-19 19:17:17 -0600

    Add test case showing code won't panic

---
+++ examples/no_panic.rs
@@ -0,0 +1,26 @@
+use highway::{HighwayHash, PortableHash};
+use std::io::Read;
+
+// Using debug_assertions as a poor man's way to omit no_panic compilation on
+// unoptimized builds.
+#[cfg_attr(not(debug_assertions), no_panic::no_panic)]
+#[inline(never)]
+fn hash_data<H: HighwayHash>(mut hasher: H, data: &[u8]) -> u64 {
+    hasher.append(data);
+    hasher.finalize64()
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut data = Vec::new();
+    stdin.lock().read_to_end(&mut data).unwrap();
+    let hasher = PortableHash::default();
+    println!("{}", hash_data(hasher, &data));
+
+    #[cfg(target_arch = "x86_64")]
+    {
+        if let Some(hasher) = highway::AvxHash::new(highway::Key::default()) {
+            println!("{}", hash_data(hasher, &data));
+        }
+    }
+}
